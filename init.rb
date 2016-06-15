2b1f266677166e0bd0131ae8b3f8d4eff00ac562
Павел Шепель <tacid@tacid.kiev.ua>
2016-06-16 02:06:11 +0300

    version bump to 1.0.0

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_ckeditor_hotkey do
   name 'Redmine CKEditor hotkey'
   author 'Tacid'
   description 'This plugin adds some hotkeys for CKEditor'
-  version '0.2.1'
   url 'https://github.com/tacid/redmine_ckeditor_hotkey'
+  version '1.0.0'
   author_url 'https://github.com/tacid'
 end
 
